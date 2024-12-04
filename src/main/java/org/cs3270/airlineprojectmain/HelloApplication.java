98fce0f3f4f1c45bc850eaffd33079dca06e913b
Ajdin Seho <ajdinseho@hotmail.com>
2024-12-04 14:57:27 -0500

    ;

--- src/main/java/org/cs3270/airlineprojectmain/HelloApplication.java
+++ src/main/java/org/cs3270/airlineprojectmain/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage firstStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginScene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         firstStage.setTitle("FirstStage");
         firstStage.setScene(scene);
         firstStage.show();
