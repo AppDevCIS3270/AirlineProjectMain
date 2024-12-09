f819e989be7a8928ef659d2f826ba908fdcc2790
Ammar Issa <ammarissa96@gmail.com>
2024-12-09 00:23:12 -0500

    admin

--- src/main/java/org/cs3270/airlineprojectmain/AdminDashboard.java
+++ src/main/java/org/cs3270/airlineprojectmain/AdminDashboard.java
@@ -1,4 +1,14 @@
 package org.cs3270.airlineprojectmain;
 
-public class AdminDashboard {
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminDashboard extends DashboardController {
+    static Scene scene;
+    static Stage window = new Stage();
+
+public static void AdminMenuScreen() throws Exception {
+    window.setTitle("Main Menu");
+
+}
 }
